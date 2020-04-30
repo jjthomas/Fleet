cfbeda8856893d8cc5a3d1f608d293580d1f1a6b
James Thomas <jamesjoethomas@gmail.com>
2020-04-29 22:29:58 -0700

    TopArbiter

--- src/main/scala/edu/stanford/fleet/StreamingWrapper.scala
+++ src/main/scala/edu/stanford/fleet/StreamingWrapper.scala
@@ -26,7 +26,8 @@ class AddrProcessingUnitIO(transferSize: Int, addrWidth: Int) extends Bundle {
   val outputReady = Input(Bool())
 
   val barrierRequest = Output(Bool())
-  val barrierCleared = Input(Bool()) // PU should wait for barrierCleared to drop low again before proceeding
+  val barrierCleared = Input(Bool()) // PU should wait for barrierCleared to drop low again before proceeding (but
+  // should clear the request immediately)
   val finished = Output(Bool())
   val coreId = Input(UInt(10.W))
 }
@@ -201,48 +202,34 @@ class CoreIOBuffer(lineBits: Int, transferSize: Int, addrWidth: Int) extends Mod
   io.core.barrierCleared := io.external.barrierCleared
 }
 
-class AXI(busWidth: Int) extends Bundle {
+class MiddleArbiterIO(axiBusWidth: Int) extends Bundle {
   val inputMemAddr = Output(UInt(64.W))
   val inputMemAddrValid = Output(Bool())
-  val inputMemAddrLen = Output(UInt(8.W))
-  val inputMemAddrReady = Input(Bool())
-  val inputMemBlock = Input(UInt(busWidth.W))
+  val inputMemBlock = Input(UInt(axiBusWidth.W))
   val inputMemBlockValid = Input(Bool())
-  val inputMemBlockReady = Output(Bool())
   val outputMemAddr = Output(UInt(64.W))
   val outputMemAddrValid = Output(Bool())
-  val outputMemAddrLen = Output(UInt(8.W))
-  val outputMemAddrId = Output(UInt(16.W))
-  val outputMemAddrReady = Input(Bool())
-  val outputMemBlock = Output(UInt(busWidth.W))
-  val outputMemStrb = Output(UInt((busWidth / 8).W))
-  val outputMemBlockValid = Output(Bool())
-  val outputMemBlockLast = Output(Bool())
+  val outputMemBlock = Output(UInt(axiBusWidth.W))
+  val outputMemStrb = Output(UInt((axiBusWidth / 8).W))
   val outputMemBlockReady = Input(Bool())
   val finished = Output(Bool())
+  val barrierRequest = Output(Bool())
+  val barrierCleared = Input(Bool())
 }
 
 class MiddleArbiter(axiBusWidth: Int, lineBits: Int, transferSize: Int, addrWidth: Int, numCores: Int, coreOffset: Int)
   extends Module {
   val io = IO(new Bundle {
     val cores = Vec(numCores, Flipped(new CoreIOBufferIO(transferSize, addrWidth)))
-    val externalAxi = new AXI(axiBusWidth)
-    val barrierRequest = Output(Bool())
-    val barrierCleared = Input(Bool())
+    val external = new MiddleArbiterIO(axiBusWidth)
   })
 
   for (i <- 0 until numCores) {
     io.cores(i).coreId := (coreOffset + i).U
-    io.cores(i).barrierCleared := io.barrierCleared
+    io.cores(i).barrierCleared := io.external.barrierCleared
   }
-  io.barrierRequest := RegNext(io.cores.map(_.barrierRequest).foldLeft(true.B)(_ && _), false.B)
-  io.externalAxi.finished := RegNext(io.cores.map(_.finished).foldLeft(true.B)(_ && _), false.B)
-
-  // these signals don't need to be set at this level
-  io.externalAxi.inputMemAddrLen := (lineBits / axiBusWidth).U
-  io.externalAxi.outputMemAddrLen := (lineBits / axiBusWidth).U
-  io.externalAxi.outputMemAddrId := 0.U
-  io.externalAxi.outputMemBlockLast := false.B
+  io.external.barrierRequest := RegNext(io.cores.map(_.barrierRequest).foldLeft(true.B)(_ && _), false.B)
+  io.external.finished := RegNext(io.cores.map(_.finished).foldLeft(true.B)(_ && _), false.B)
 
   val coreSelecting :: addressReading :: dataFetching :: dataFlushing :: Nil = util.Enum(4)
 
@@ -254,9 +241,8 @@ class MiddleArbiter(axiBusWidth: Int, lineBits: Int, transferSize: Int, addrWidt
     val inputAddr = Reg(UInt(addrWidth.W))
     val inputData = Reg(UInt(lineBits.W))
 
-    io.externalAxi.inputMemAddr := inputAddr
-    io.externalAxi.inputMemAddrValid := inputState === dataFetching
-    io.externalAxi.inputMemBlockReady := inputState === dataFetching
+    io.external.inputMemAddr := inputAddr
+    io.external.inputMemAddrValid := inputState === dataFetching
 
     for (i <- 0 until numCores) {
       io.cores(i).inputTransfer := inputData(transferSize - 1, 0)
@@ -278,8 +264,8 @@ class MiddleArbiter(axiBusWidth: Int, lineBits: Int, transferSize: Int, addrWidt
         inputState := dataFetching
       }
       is(dataFetching) {
-        when(io.externalAxi.inputMemBlockValid) {
-          inputData := io.externalAxi.inputMemBlock ## inputData(lineBits - 1, axiBusWidth)
+        when(io.external.inputMemBlockValid) {
+          inputData := io.external.inputMemBlock ## inputData(lineBits - 1, axiBusWidth)
           when(inputFetchCounter === (lineBits / axiBusWidth - 1).U) {
             inputFetchCounter := 0.U
             inputState := dataFlushing
@@ -308,11 +294,10 @@ class MiddleArbiter(axiBusWidth: Int, lineBits: Int, transferSize: Int, addrWidt
   val outputData = Reg(UInt(lineBits.W))
   val outputStrb = Reg(UInt((lineBits / 8).W))
 
-  io.externalAxi.outputMemAddr := outputAddr
-  io.externalAxi.outputMemAddrValid := outputState === dataFlushing
-  io.externalAxi.outputMemBlockValid := outputState === dataFlushing
-  io.externalAxi.outputMemBlock := outputData(axiBusWidth - 1, 0)
-  io.externalAxi.outputMemStrb := outputStrb(axiBusWidth / 8 - 1, 0)
+  io.external.outputMemAddr := outputAddr
+  io.external.outputMemAddrValid := outputState === dataFlushing
+  io.external.outputMemBlock := outputData(axiBusWidth - 1, 0)
+  io.external.outputMemStrb := outputStrb(axiBusWidth / 8 - 1, 0)
 
   for (i <- 0 until numCores) {
     io.cores(i).outputReady := (outputState === dataFetching) && (outputCore === i.U)
@@ -343,7 +328,7 @@ class MiddleArbiter(axiBusWidth: Int, lineBits: Int, transferSize: Int, addrWidt
       }
     }
     is(dataFlushing) {
-      when (io.externalAxi.outputMemBlockReady) {
+      when (io.external.outputMemBlockReady) {
         outputData := outputData(lineBits - 1, axiBusWidth)
         outputStrb := outputStrb(lineBits / 8 - 1, axiBusWidth / 8)
         when(outputFlushCounter === (lineBits / axiBusWidth - 1).U) {
@@ -353,6 +338,138 @@ class MiddleArbiter(axiBusWidth: Int, lineBits: Int, transferSize: Int, addrWidt
           outputFlushCounter := outputFlushCounter + 1.U
         }
       }
+    }
+  }
+}
+
+class AXI(busWidth: Int) extends Bundle {
+  val inputMemAddr = Output(UInt(64.W))
+  val inputMemAddrValid = Output(Bool())
+  val inputMemAddrLen = Output(UInt(8.W))
+  val inputMemAddrReady = Input(Bool())
+  val inputMemBlock = Input(UInt(busWidth.W))
+  val inputMemBlockValid = Input(Bool())
+  val inputMemBlockReady = Output(Bool())
+  val outputMemAddr = Output(UInt(64.W))
+  val outputMemAddrValid = Output(Bool())
+  val outputMemAddrLen = Output(UInt(8.W))
+  val outputMemAddrId = Output(UInt(16.W))
+  val outputMemAddrReady = Input(Bool())
+  val outputMemBlock = Output(UInt(busWidth.W))
+  val outputMemStrb = Output(UInt((busWidth / 8).W))
+  val outputMemBlockValid = Output(Bool())
+  val outputMemBlockLast = Output(Bool())
+  val outputMemBlockReady = Input(Bool())
+  val finished = Output(Bool())
+}
+
+class TopArbiter(axiBusWidth: Int, lineBits: Int, numChildren: Int) extends Module {
+  val io = IO(new Bundle {
+    val children = Vec(numChildren, Flipped(new MiddleArbiterIO(axiBusWidth)))
+    val external = new AXI(axiBusWidth)
+  })
+
+  val barrierCleared = WireInit(RegNext(io.children.map(_.barrierRequest).foldLeft(true.B)(_ && _), false.B))
+  for (i <- 0 until numChildren) {
+    io.children(i).barrierCleared := barrierCleared
+  }
+  io.external.finished := RegNext(io.children.map(_.finished).foldLeft(true.B)(_ && _), false.B)
+
+  io.external.inputMemAddrLen := (lineBits / axiBusWidth - 1).U
+  io.external.outputMemAddrLen := (lineBits / axiBusWidth - 1).U
+
+  { // isolate from output logic
+    val inputReqInFlight = RegInit(VecInit(Seq.fill(numChildren)(false.B)))
+    val inputAddrPtr = RegInit(0.U(log2Ceil(numChildren).W))
+    val inputDataPtr = RegInit(0.U(log2Ceil(numChildren).W))
+    val inputDataCounter = RegInit(0.U(log2Ceil(lineBits / axiBusWidth).W))
+
+    io.external.inputMemAddr := io.children(inputAddrPtr).inputMemAddr
+    io.external.inputMemAddrValid := io.children(inputAddrPtr).inputMemAddrValid && !inputReqInFlight(inputAddrPtr)
+
+    when(!io.children(inputAddrPtr).inputMemAddrValid || // if !inputMemAddrValid then !inputReqInFlight
+      (io.external.inputMemAddrValid && io.external.inputMemAddrReady)) {
+      when(inputAddrPtr === (numChildren - 1).U) {
+        inputAddrPtr := 0.U
+      }.otherwise {
+        inputAddrPtr := inputAddrPtr + 1.U
+      }
+    }
+    when(io.external.inputMemAddrValid && io.external.inputMemAddrReady) {
+      inputReqInFlight(inputAddrPtr) := true.B
+    }
+
+    io.external.inputMemBlockReady := inputReqInFlight(inputDataPtr)
+    for (i <- 0 until numChildren) {
+      io.children(i).inputMemBlock := io.external.inputMemBlock
+      io.children(i).inputMemBlockValid := io.external.inputMemBlockValid && io.external.inputMemBlockReady &&
+        inputDataPtr === i.U
+    }
+
+    when(!inputReqInFlight(inputDataPtr) || (io.external.inputMemBlockValid && io.external.inputMemBlockReady &&
+      inputDataCounter === (lineBits / axiBusWidth - 1).U)) {
+      when(inputDataPtr === (numChildren - 1).U) {
+        inputDataPtr := 0.U
+      }.otherwise {
+        inputDataPtr := inputDataPtr + 1.U
+      }
+    }
+    when(io.external.inputMemBlockValid && io.external.inputMemBlockReady) {
+      when(inputDataCounter === (lineBits / axiBusWidth - 1).U) {
+        inputDataCounter := 0.U
+        inputReqInFlight(inputDataPtr) := false.B
+      }.otherwise {
+        inputDataCounter := inputDataCounter + 1.U
+      }
+    }
+  }
+
+  val outputReqInFlight = RegInit(VecInit(Seq.fill(numChildren)(false.B)))
+  val outputAddrPtr = RegInit(0.U(log2Ceil(numChildren).W))
+  val outputAddrId = RegInit(0.U(16.W))
+  val outputDataPtr = RegInit(0.U(log2Ceil(numChildren).W))
+  val outputDataCounter = RegInit(0.U(log2Ceil(lineBits / axiBusWidth).W))
+
+  io.external.outputMemAddr := io.children(outputAddrPtr).outputMemAddr
+  io.external.outputMemAddrValid := io.children(outputAddrPtr).outputMemAddrValid && !outputReqInFlight(outputAddrPtr)
+  io.external.outputMemAddrId := outputAddrId
+
+  when (!io.children(outputAddrPtr).outputMemAddrValid || // if !outputMemAddrValid then !outputReqInFlight
+    (io.external.outputMemAddrValid && io.external.outputMemAddrReady)) {
+    when (outputAddrPtr === (numChildren - 1).U) {
+      outputAddrPtr := 0.U
+    } .otherwise {
+      outputAddrPtr := outputAddrPtr + 1.U
+    }
+  }
+  when (io.external.outputMemAddrValid && io.external.outputMemAddrReady) {
+    outputReqInFlight(outputAddrPtr) := true.B
+    outputAddrId := outputAddrId + 1.U
+  }
+
+  io.external.outputMemBlockValid := outputReqInFlight(outputDataPtr)
+  io.external.outputMemBlock := io.children(outputDataPtr).outputMemBlock
+  io.external.outputMemStrb := io.children(outputDataPtr).outputMemStrb
+  io.external.outputMemBlockLast := outputDataCounter === (lineBits / axiBusWidth - 1).U
+  for (i <- 0 until numChildren) {
+    io.children(i).outputMemBlockReady := io.external.outputMemBlockValid && io.external.outputMemBlockReady &&
+      outputDataPtr === i.U
+  }
+
+  when (!outputReqInFlight(outputDataPtr) || (io.external.outputMemBlockValid && io.external.outputMemBlockReady &&
+    outputDataCounter === (lineBits / axiBusWidth - 1).U)) {
+    when (outputDataPtr === (numChildren - 1).U) {
+      outputDataPtr := 0.U
+    } .otherwise {
+      outputDataPtr := outputDataPtr + 1.U
+    }
+  }
+  when (io.external.outputMemBlockValid && io.external.outputMemBlockReady) {
+    when (outputDataCounter === (lineBits / axiBusWidth - 1).U) {
+      outputDataCounter := 0.U
+      outputReqInFlight(outputDataPtr) := false.B
+    } .otherwise {
+      outputDataCounter := outputDataCounter + 1.U
     }
   }
 }
